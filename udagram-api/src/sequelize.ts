56b259f9d7a6fa93c3e59926317d46bddaf871a6
Stefan Rausch <rausch.stefan@edumail.at>
2021-05-31 08:55:41 +0200

    Add sequelize for RDS

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,4 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.pg_port,
-
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${config.pg_port}/${config.database}`);
